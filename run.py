d6873fb1d3cd3469015ce0e018aac4d182f086da
DigitalTeam <teamnr70@gmail.com>
2018-12-16 15:35:53 +0700

    Update run.py

--- run.py
+++ run.py
@@ -369,5 +369,6 @@ while True:
     time.sleep(0.1)
 
   # Start forwarder
-  subprocess.call(['/opt/ttn-gateway/mp_pkt_fwd', '-c', '/opt/ttn-gateway/', '-s', os.getenv('SPI_SPEED', '8000000')])
+  #subprocess.call(['/opt/ttn-gateway/mp_pkt_fwd', '-c', '/opt/ttn-gateway/', '-s', os.getenv('SPI_SPEED', '8000000')])
+  subprocess.call(['/opt/ttn-gateway/mp_pkt_fwd', '-c', '/opt/ttn-gateway/'])
   time.sleep(15)
